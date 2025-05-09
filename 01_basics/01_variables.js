ee4c9456e88938db8b7c83c7f1e682b0dfd2ae1a
Ankit raj <nkit328@gmail.com>
2025-05-09 05:35:00 +0000

    container done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 144553
+let accountEmail = "nkit328@gmail.com"
+var accountPassword ="1234"
+accountCity ="jaipur"
+console.table([accountId,accountEmail,accountPassword])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+alert(3 + 3) //we are using nodejs not browsing
+
+
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean =>true/false
+// null => standlone value
+// undefined =>
+// symbol =>unique
+
+//object 
+console.log(typeof age);
